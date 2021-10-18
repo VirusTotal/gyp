f2aa8e38678c7e3994dbc8db0aa9252f7faeb99d
Zohiartze <zohiartze@virustotal.com>
2021-10-18 16:05:18 +0200

    Address review suggestions

--- ast/traversal.go
+++ ast/traversal.go
@@ -2,13 +2,13 @@ package ast
 
 // searchCallback is the kind of function passed to DepthFirstSearch to get
 // notified about the traversed nodes in a gyp ast tree.
-type searchCallback func(expr Node, cbParam interface{})
+type searchCallback func(expr Node)
 
 // DepthFirstSearch performs a depth-first traversal of expr's syntax tree and
 // invokes callback with the given cbParam for every node found.
-func DepthFirstSearch(expr Node, callback searchCallback, cbParam interface{}) {
-	callback(expr, cbParam)
+func DepthFirstSearch(expr Node, callback searchCallback) {
+	callback(expr)
 	for _, expr := range expr.Children() {
-		DepthFirstSearch(expr, callback, cbParam)
+		DepthFirstSearch(expr, callback)
 	}
 }
