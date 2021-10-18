7eeb03bfec0e8f0faaa6ad380968d11b37870ece
zori <40301647+Zohiartze@users.noreply.github.com>
2021-10-18 17:03:12 +0200

    Add function for ast node traversal (#41)

---
+++ ast/traversal.go
@@ -0,0 +1,14 @@
+package ast
+
+// searchCallback is the kind of function passed to DepthFirstSearch to get
+// notified about the traversed nodes in a gyp ast tree.
+type searchCallback func(expr Node)
+
+// DepthFirstSearch performs a depth-first traversal of expr's syntax tree and
+// invokes callback for every node found.
+func DepthFirstSearch(expr Node, callback searchCallback) {
+	callback(expr)
+	for _, expr := range expr.Children() {
+		DepthFirstSearch(expr, callback)
+	}
+}
