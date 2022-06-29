7bfa2231c8953a0f504a4a52d0dd9e69a613010b
Carlos Pacho <cpacho@fb.com>
2022-06-28 20:25:11 -0400

    Update docs for GetDependencyChainForRules to not mention returnDepAsPrivate

--- utils/dependency_walker.go
+++ utils/dependency_walker.go
@@ -14,7 +14,6 @@ type queueT struct {
 }
 
 // GetDependencyChainForRules will recursively find dependencies for a list of rules
-// returnDepAsPrivate will dictate if dependent rules are marked as private
 func GetDependencyChainForRules(ruleset ast.RuleSet, ruleNames ...string) (ast.RuleSet, error) {
 	queue := make(map[string]struct{}) // List of rules to get dependencies for
 	processed := []string{}            // List of rules where dependencies have already been found
